f9af8513a71aef0383c000086f8c3b76933395b5
Deniel <denielaz2020@gmail.com>
2025-12-26 16:06:57 +0100

    Fix vite config

--- vite.config.js
+++ vite.config.js
@@ -2,4 +2,12 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
   base: '/snow-game/',
+  optimizeDeps: {
+    include: ['three']
+  },
+  build: {
+    rollupOptions: {
+      external: []
+    }
+  }
 });
